37732c481c6330b7d1ade60aaab02dc2695302f2
basu sahu <basusahu1201@gmail.com>
2023-12-19 23:11:09 +0530

    Array problem

--- src/program4.java
+++ src/program4.java
@@ -1,29 +1,23 @@
 import java.util.*;
 public class program4 {
-	 public static void main(String[] args) {
-	        // Declare variables for total number and an array of integers.
-	        int total_num;
-	        int[] numbers = new int[]{1, 2, 3, 4, 6, 7};
-
-	        // Assign the value 7 to the variable total_num.
-	        total_num = 7;
-
-	        // Calculate the expected sum of numbers using the arithmetic sum formula.
-	        int expected_num_sum = total_num * ((total_num + 1) / 2);
-
-	        // Initialize a variable to store the sum of numbers.
-	        int num_sum = 0;
-
-	        // Iterate through the elements of the 'numbers' array.
-	        for (int i : numbers) {
-	            // Add each element to the 'num_sum' variable.
-	            num_sum += i;
-	        }
-
-	        // Calculate the missing number by subtracting 'num_sum' from 'expected_num_sum'.
-	        System.out.print(expected_num_sum - num_sum);
-
-	        // Print a newline character to end the line.
-	        System.out.print("\n");
-	    }
+	public static void main(String[] args) {
+		int [] arr = {23,43, 53,13,15};
+		for(int i =0; i<=arr.length-2; i++)
+		{
+			for(int j=0; j<arr.length-1-i ;  j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int  temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		for(int x : arr)
+		{
+			System.out.println(x);
+		}
+		
+	}
 }

21a5229b87e1fc0a8d45ae1e683ec28f18f6eac4
basu sahu <basusahu1201@gmail.com>
2023-12-23 23:24:03 +0530

    String problem

---
+++ src/com/twodArray/java/program3.java
@@ -0,0 +1,15 @@
+package com.twodArray.java;
+import java.util.*;
+public class program3 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("enter the no.of class and student");
+	String [][] arr = new String [scan.nextInt()][];
+	
+	for(int i =0; i<=arr.length-1; i++)
+	{
+	System.out.println("enter the no. of student in class" + (i+1));
+	arr [(scan.nextInt());
+	}
+}
+}

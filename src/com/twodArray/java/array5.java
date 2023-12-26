f1ee4e4db6b84ed254e5c6c2c36dd5073c8518fe
basu sahu <basusahu1201@gmail.com>
2023-12-26 22:40:13 +0530

    Array problem

---
+++ src/com/twodArray/java/array5.java
@@ -0,0 +1,27 @@
+package com.twodArray.java;
+import java.util.*;
+public class array5 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("enter the length of array");
+	int [] arr1 = new int [scan.nextInt()];
+	int a1= arr1.length;
+	for(int i = 0 ; i<=arr1.length-1; i++)
+	{
+		arr1[i] =  scan.nextInt();
+	}
+
+	for(int x:arr1)
+	{
+		System.out.println(x);
+	}
+	int y = arr1.length;
+	System.out.println(y);
+	
+}
+}
+
+
+	
+
+

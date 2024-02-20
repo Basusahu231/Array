d6751a0e022b4899c2aeef6d33a75d4eb6015e1b
basu sahu <basusahu1201@gmail.com>
2024-02-20 23:58:40 +0530

    Array Problem

--- src/com/practice/java/program15.java
+++ src/com/practice/java/program15.java
@@ -1,21 +1,15 @@
 package com.practice.java;
 
 public class program15 {
-	   public static void reverseArrayExtraArray(int[] arr) {
-	        int[] reversedArr = new int[arr.length];
-	        for (int i = 0; i < arr.length; i++) {
-	            reversedArr[i] = arr[arr.length - i - 1];
-	        }
-	 
-	        // Print reversed array
-	        System.out.print("Reversed Array: ");
-	        for (int i : reversedArr) {
-	            System.out.print(i + " ");
-	        }
-	    }
-	 
-	    public static void main(String[] args) {
-	        int[] originalArr = {1, 2, 3, 4, 5};
-	        reverseArrayExtraArray(originalArr);
-	    }
+	public static void main(String[] args) {
+		int a = 10 , b =5, temp =1;
+		for(int i =1; i<=a && i<= b; i++)
+		{
+			if(a%i==0 && b%i==0)
+			{
+				temp =i;
+			}
+		}
+		System.out.println(temp);
+	}
 }
